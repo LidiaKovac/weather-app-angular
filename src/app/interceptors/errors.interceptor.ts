c261db8c359795ae4cea1ac96532c7a6a17e41ae
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-08-16 11:34:54 +0200

    add better errors

--- src/app/interceptors/errors.interceptor.ts
+++ src/app/interceptors/errors.interceptor.ts
@@ -6,17 +6,21 @@ import {
   HttpInterceptor,
   HttpErrorResponse
 } from '@angular/common/http';
-import { BehaviorSubject, Observable, catchError, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, map, throwError } from 'rxjs';
 import { ApiService } from '../services/api.service';
-import { ErrorsService } from '../services/errors.service';
+import { ErrorsService } from "../services/errors.service";
 
 @Injectable()
 export class ErrorsInterceptor implements HttpInterceptor {
   constructor(private errorSrv:ErrorsService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request).pipe(catchError((error: HttpErrorResponse) => {
-      this.errorSrv.throw(`Error! ${error.status} - ${error.statusText}`, 2000)
+    return next.handle(request).pipe(map((res)=> {
+      console.log(res)
+      return res
+    }), catchError((error: HttpErrorResponse) => {
+      console.log("erro", error)
+      this.errorSrv.throw(`Error! ${error.error} `, 2000)
       return throwError(() => new Error(error.message))
     }))
   }
